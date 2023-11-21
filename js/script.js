7476f73ce26751511278e8da6f0d8bee7e5926b7
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-21 20:56:27 +0100

    [FINISH]  finish exercise

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@
 
  button.addEventListener("click", function(){
 
+    /* input e crezione contenitori */
+    let nameSurname= document.getElementById("nome_cognome").value;
     let numero_km = document.getElementById("numkm").value;
     let eta = document.getElementById("etapers").value;
     
+    /* calcolo del prezzo */
     let prezzo= numero_km * 0.21;
-    
+   
     if (eta == "fino17"){
         prezzo = prezzo - (prezzo * 0.2);
         
@@ -19,11 +22,20 @@
         prezzo = prezzo - prezzo * 0.4;    
     }
 
+   
+     document.getElementById("name_passanger").innerHTML = " ";  /* svuoto il contenitorte in modo che ad ogni ricalcolo non faccia l aggiunta del nome  */
+   
+    /*modifichiamo il risultato in modo che bbai 2 numeri dopo la virgola  */
     prezzo= prezzo.toFixed(2);
-    console.log(prezzo);
-     
+    
+
+    document.getElementById("costo").innerHTML= prezzo +"€";
+    document.getElementById("name_passanger").innerHTML = nameSurname;
+
  })
 
+
+ /* fuonzionamento bottone cancella */
  const buttondue= document.getElementById("reset");
  buttondue.addEventListener("click", function(){
     document.getElementById("nome_cognome").value=" ";
@@ -31,6 +43,7 @@
     document.getElementById("etapers").value="iniziale";
 })
 
+ 
 
  
 
