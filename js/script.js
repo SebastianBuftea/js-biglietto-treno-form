9c31baf2c8c5382e4791def43a2a4ca649fbbe5f
SebastianBuftea <sebibuftea85@gmail.com>
2023-11-21 15:17:45 +0100

    [UPDATE] finish milestone 1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+ const button= document.getElementById("calcola");
+
+ button.addEventListener("click", function(){
+    
+    let numero_km = document.getElementById("numkm").value;
+    let eta = document.getElementById("etapers").value;
+    let prezzo= numero_km * 0.21;
+    
+    if (eta < 18){
+        prezzo = prezzo - (prezzo * 0.2);
+          
+    }
+    else if(eta >= 65){
+        prezzo = prezzo - prezzo * 0.6;    
+    }
+    prezzo= prezzo.toFixed(2);
+    console.log(prezzo);
+ 
+ })
+
+
+
